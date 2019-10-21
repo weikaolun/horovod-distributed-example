36c4d98e8c8658a9ebf9829b62065ae724a2a90d
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2019-10-21 23:39:35 +0200

    updated example to support latest horovod image

--- main.py
+++ main.py
@@ -2,17 +2,22 @@
 # Copyright (c) 2019 Paperspace Inc.
 # Author: Michal Kulaczkowski
 
-import os
 import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-import horovod.tensorflow as hvd
+import os
+import errno
+import numpy as np
 import tensorflow as tf
+import horovod.tensorflow as hvd
+
+from tensorflow import keras
+
+tf.logging.set_verbosity(tf.logging.INFO)
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, FILE_DIR)
 
-from utils import train_dataset, test_dataset
 
 
 def parse_args():
@@ -74,104 +79,211 @@ def parse_args():
     return opts
 
 
-def get_input_fn(opts, is_train=True):
-    """Returns input_fn.  is_train=True shuffles and repeats data indefinitely"""
+def cnn_model_fn(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+    # MNIST images are 28x28 pixels, and have one color channel
+    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
-    def input_fn():
-        with tf.device('/cpu:0'):
-            if is_train:
-                dataset = train_dataset(opts.data_dir, fashion=opts.fashion)
-                dataset = dataset.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=5 * opts.batch_size, count=None))
-            else:
-                dataset = test_dataset(opts.data_dir, fashion=opts.fashion)
-            dataset = dataset.batch(batch_size=opts.batch_size)
-            iterator = dataset.make_one_shot_iterator()
-            return iterator.get_next()
+    # Convolutional Layer #1
+    # Computes 32 features using a 5x5 filter with ReLU activation.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 28, 28, 1]
+    # Output Tensor Shape: [batch_size, 28, 28, 32]
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
 
-    return input_fn
+    # Pooling Layer #1
+    # First max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 28, 28, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 32]
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
+    # Convolutional Layer #2
+    # Computes 64 features using a 5x5 filter.
+    # Padding is added to preserve width and height.
+    # Input Tensor Shape: [batch_size, 14, 14, 32]
+    # Output Tensor Shape: [batch_size, 14, 14, 64]
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
 
-def cnn_net(input_tensor, opts):
-    """Return logits output from CNN net"""
-    temp = tf.reshape(input_tensor, shape=(-1, 28, 28, 1), name='input_image')
-    for i, n_units in enumerate(opts.hidden_units):
-        temp = tf.layers.conv2d(temp, filters=n_units, kernel_size=opts.kernel_size, strides=(2, 2),
-                                activation=tf.nn.relu, name='cnn' + str(i))
-        temp = tf.layers.dropout(temp, rate=opts.dropout)
-    temp = tf.reduce_mean(temp, axis=(2, 3), keepdims=False, name='average')
-    return tf.layers.dense(temp, 10)
+    # Pooling Layer #2
+    # Second max pooling layer with a 2x2 filter and stride of 2
+    # Input Tensor Shape: [batch_size, 14, 14, 64]
+    # Output Tensor Shape: [batch_size, 7, 7, 64]
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
+    # Flatten tensor into a batch of vectors
+    # Input Tensor Shape: [batch_size, 7, 7, 64]
+    # Output Tensor Shape: [batch_size, 7 * 7 * 64]
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
 
-def get_model_fn(opts):
-    """Return model fn to be used for Estimator class"""
+    # Dense Layer
+    # Densely connected layer with 1024 neurons
+    # Input Tensor Shape: [batch_size, 7 * 7 * 64]
+    # Output Tensor Shape: [batch_size, 1024]
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024,
+                            activation=tf.nn.relu)
 
-    def model_fn(features, labels, mode):
-        """Returns EstimatorSpec for different mode (train/eval/predict)"""
-        logits = cnn_net(features, opts)
-        pred = tf.cast(tf.argmax(logits, axis=1), tf.int64)
-        if mode == tf.estimator.ModeKeys.PREDICT:
-            return tf.estimator.EstimatorSpec(mode, predictions={'logits': logits, 'pred': pred})
+    # Add dropout operation; 0.6 probability that element will be kept
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-        cent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='cross_entropy')
-        loss = tf.reduce_mean(cent, name='loss')
+    # Logits layer
+    # Input Tensor Shape: [batch_size, 1024]
+    # Output Tensor Shape: [batch_size, 10]
+    logits = tf.layers.dense(inputs=dropout, units=10)
 
-        metrics = {'accuracy': tf.metrics.accuracy(labels=labels, predictions=pred, name='accuracy')}
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-        if mode == tf.estimator.ModeKeys.EVAL:
-            return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=onehot_labels, logits=logits)
 
-        lr = tf.train.exponential_decay(learning_rate=opts.learning_rate * hvd.size(),
-                                        global_step=tf.train.get_global_step(),
-                                        decay_steps=1,
-                                        decay_rate=1.0 - opts.learning_decay)
-        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        # Horovod: scale learning rate by the number of workers.
+        optimizer = tf.train.MomentumOptimizer(
+            learning_rate=0.001 * hvd.size(), momentum=0.9)
+
+        # Horovod: add Horovod Distributed Optimizer.
         optimizer = hvd.DistributedOptimizer(optimizer)
-        train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-        if mode == tf.estimator.ModeKeys.TRAIN:
-            return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
-    return model_fn
+        # Save accuracy scalar to Tensorboard output.
+        accuracy = tf.metrics.accuracy(
+            labels=labels, predictions=tf.argmax(logits, axis=1))
 
+        tf.summary.scalar('train_accuracy', accuracy[1])
 
-def main(opts):
-    """Main"""
-    hvd.init()
-    tf.logging.info('Horovod size: {}'.format(hvd.size()))
-    tf.logging.info('Horovod local rank: {}'.format(hvd.local_rank()))
-    tf.logging.info('Horovod rank: {}'.format(hvd.rank()))
+        tf.summary.scalar('loss', loss)
 
-    # Create an estimator
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss,
+                                          train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def run_mnist(flags_obj, train_data, train_labels, eval_data, eval_labels):
+
+    # Horovod: pin GPU to be used to process local rank (one GPU per process)
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     config.gpu_options.visible_device_list = str(hvd.local_rank())
 
-    runconfig = tf.estimator.RunConfig(
-        model_dir=opts.log_dir,
-        save_summary_steps=opts.save_summary_steps,
-        save_checkpoints_steps=opts.ckpt_steps,
-        keep_checkpoint_max=opts.max_ckpts,
-        log_step_count_steps=opts.log_step_count_steps,
-        session_config=config)
+    # Horovod: save checkpoints only on worker 0 to prevent other workers from
+    # corrupting them.
+    model_dir = os.path.abspath(os.environ.get('PS_MODEL_PATH', os.getcwd() + '/models') + '/horovod-mnist')
 
-    estimator = tf.estimator.Estimator(
-        model_fn=get_model_fn(opts),
-        config=runconfig)
+    # Create the Estimator
+    mnist_classifier = tf.estimator.Estimator(
+        model_fn=cnn_model_fn,
+        model_dir=model_dir,
+        config=tf.estimator.RunConfig(session_config=config))
 
+    # Set up logging for predictions
+    # Log the values in the "Softmax" tensor with label "probabilities"
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=500)
+
+    # Horovod: BroadcastGlobalVariablesHook broadcasts initial variable states from
+    # rank 0 to all other processes. This is necessary to ensure consistent
+    # initialization of all workers when training is started with random weights or
+    # restored from a checkpoint.
     bcast_hook = hvd.BroadcastGlobalVariablesHook(0)
-    training_hooks = [bcast_hook]
 
-    # Create input fn
-    train_input_fn = get_input_fn(opts, is_train=True)
-    eval_input_fn = get_input_fn(opts, is_train=False)
+    # Train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True)
 
-    for _ in range(opts.max_steps//opts.eval_steps):
-        estimator.train(
-            input_fn=train_input_fn,
-            steps=opts.eval_steps,
-            hooks=training_hooks)
+    # Horovod: adjust number of steps based on number of GPUs.
+    mnist_classifier.train(
+        input_fn=train_input_fn,
+        steps=20000 // hvd.size(),
+        hooks=[logging_hook, bcast_hook])
 
-        estimator.evaluate(
-            input_fn=eval_input_fn)
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+
+    if flags_obj.export_dir:
+        tf.logging.debug('Starting to Export model to {}'.format(str(flags_obj.export_dir)))
+        image = tf.placeholder(tf.float32, [None, 28, 28])
+        input_fn = tf.estimator.export.build_raw_serving_input_receiver_fn({
+            'image': image,
+        })
+        mnist_classifier.export_savedmodel(flags_obj.export_dir, input_fn,
+                                           strip_default_attrs=True)
+        tf.logging.debug('Model Exported')
+
+    return eval_results
+
+
+def main(args):
+    # Horovod: initialize Horovod.
+    hvd.init()
+
+    # Keras automatically creates a cache directory in ~/.keras/datasets for
+    # storing the downloaded MNIST data. This creates a race
+    # condition among the workers that share the same filesystem. If the
+    # directory already exists by the time this worker gets around to creating
+    # it, ignore the resulting exception and continue.
+    cache_dir = os.path.join(os.path.expanduser('~'), '.keras', 'datasets')
+    if not os.path.exists(cache_dir):
+        try:
+            os.mkdir(cache_dir)
+        except OSError as e:
+            if e.errno == errno.EEXIST and os.path.isdir(cache_dir):
+                pass
+            else:
+                raise
+
+    # Download and load MNIST dataset.
+    (train_data, train_labels), (eval_data, eval_labels) = \
+        keras.datasets.mnist.load_data('MNIST-data-%d' % hvd.rank())
+
+    # The shape of downloaded data is (-1, 28, 28), hence we need to reshape it
+    # into (-1, 784) to feed into our network. Also, need to normalize the
+    # features between 0 and 1.
+    train_data = np.reshape(train_data, (-1, 784)) / 255.0
+    eval_data = np.reshape(eval_data, (-1, 784)) / 255.0
+
+    eval_results = run_mnist(args, train_data, train_labels, eval_data, eval_labels)
+
+    print(eval_results)
 
 
 if __name__ == "__main__":
